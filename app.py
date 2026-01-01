aa100cb38e0b236e7922534eebf64cda108ef725
Arthur Gabriel Gotardo Pohl <122641048+Fear-sys@users.noreply.github.com>
2026-01-01 01:22:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import requests
 from complementos.empresas import Empresa
 from complementos.ItensDaLoja.produtos import Produto
 
-answareEmpresas = requests.get('aqui entra a url da API com os dados de cadastro das empresas')
-answareProdutos = requests.get('aqui entra a url da API com os dados de cadastro dos produtos')
+answareEmpresas = requests.get('aqui vou colocar a url da API com os dados de cadastro das empresas')
+answareProdutos = requests.get('aqui vou colocar a url da API com os dados de cadastro dos produtos')
 if answareEmpresas.status_code == 200 and answareProdutos.status_code == 200:
     print(f'Your request has been complete')
     Empresas = {}
